957b39bbc036861c6ae895d71397491742e8db74
AlenaPanPanda <guiyguker@gmail.com>
2024-02-28 14:26:33 +0100

    change icon

--- app/src/main/java/com/example/simplebankmanager/fragments/TransferFundsFragment.kt
+++ app/src/main/java/com/example/simplebankmanager/fragments/TransferFundsFragment.kt
@@ -1,8 +1,6 @@
 package com.example.simplebankmanager.fragments
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.Toast
